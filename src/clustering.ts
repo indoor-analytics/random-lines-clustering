b72580cd1cf0b137cd2995450bebfbe7ee076269
Rémy Raes <remy.raes@inria.fr>
2021-03-30 10:45:41 +0200

    [docs] adding clusterPaths actions

--- src/clustering.ts
+++ src/clustering.ts
@@ -10,5 +10,10 @@ export function clusterPaths (
     zoneOfInterest: Feature<Polygon>,
     paths: Feature<LineString>[],
 ): Feature<LineString>[] {
+    // 1. Define a bounding box around the zone of interest
+    // 2. Draw random lines crossing the bounding box
+    // 3. Mark all intersections with input paths
+    // 4. Cluster intersections
+    // 5. Rebuild input paths using clustered intersections
     return [];
 }
