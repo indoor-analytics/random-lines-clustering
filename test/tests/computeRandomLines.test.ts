9b016e14f20a08c147cf9d8cac3894402e82591d
Rémy Raes <remy.raes@inria.fr>
2021-03-30 14:34:26 +0200

    [test] checking if using same seed produces same lines

--- test/tests/computeRandomLines.test.ts
+++ test/tests/computeRandomLines.test.ts
@@ -26,4 +26,18 @@ describe ('computeRandomLines options companion object', () => {
         const lines = computeRandomLines(actualFlandersRailwayBbox, {linesCount, seed: 'hola'});
         expect(lines.length).to.equal(linesCount);
     });
+
+    it ('should compute different lines with two method calls', () => {
+        const lines1 = computeRandomLines(actualFlandersRailwayBbox);
+        const lines2 = computeRandomLines(actualFlandersRailwayBbox);
+        expect(lines1.length).to.equal(lines2.length);
+        expect(lines1).to.not.deep.equal(lines2);
+    });
+
+    it ('should compute exact same lines with same seed', () => {
+        const seed = 'this_is_a_random_seed';
+        const lines1 = computeRandomLines(actualFlandersRailwayBbox, {seed});
+        const lines2 = computeRandomLines(actualFlandersRailwayBbox, {seed});
+        expect(lines1).to.deep.equal(lines2);
+    });
 });
