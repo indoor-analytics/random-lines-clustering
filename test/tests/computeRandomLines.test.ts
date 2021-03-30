c08d040afbe672e7561d62fc238639676e1eeaa7
Rémy Raes <remy.raes@inria.fr>
2021-03-30 13:53:39 +0200

    [test] computeRandomLines should produce as many lines as default option

--- test/tests/computeRandomLines.test.ts
+++ test/tests/computeRandomLines.test.ts
@@ -1,7 +1,16 @@
 import { expect } from "chai";
+import {computeRandomLines} from "../../src/randomLines/computeRandomLines";
+import {COMPUTE_RANDOM_LINES_OPTIONS_DEFAULTS} from "../../src/randomLines/ComputeRandomLinesOptions";
 
 describe ('computeRandomLines', () => {
     it ('should fail', () => {
         expect(true).to.equal(false);
+    });
+});
+
+describe ('computeRandomLines options companion object', () => {
+    it ('should produce as many lines as default option', () => {
+        const lines = computeRandomLines();
+        expect(lines.length).to.equal(COMPUTE_RANDOM_LINES_OPTIONS_DEFAULTS.linesCount);
     });
 });
