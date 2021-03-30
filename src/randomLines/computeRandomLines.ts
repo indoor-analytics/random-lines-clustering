2713662a2a9d0e15897c4a1454f555f1c02509b3
Rémy Raes <remy.raes@inria.fr>
2021-03-30 16:21:10 +0200

    [feat] not generating lines which are perimeter segments

--- src/randomLines/computeRandomLines.ts
+++ src/randomLines/computeRandomLines.ts
@@ -1,4 +1,4 @@
-import {Feature, lineString, LineString, MultiLineString, Polygon, Properties} from "@turf/helpers";
+import {Feature, lineString, LineString, MultiLineString, Point, Polygon, Properties} from "@turf/helpers";
 import {
     COMPUTE_RANDOM_LINES_OPTIONS_DEFAULTS,
     ComputeRandomLinesOptions,
@@ -7,6 +7,7 @@ import {
 import {polygonToLine} from "@turf/polygon-to-line";
 import length from "@turf/length";
 import along from "@turf/along";
+import lineSlice from "@turf/line-slice";
 const randomSeedGenerator = require('random-seed');
 
 export function computeRandomLines (
@@ -20,11 +21,22 @@ export function computeRandomLines (
     const areaPerimeter = polygonToLine(area);
     const perimeterLength = length(areaPerimeter);
 
+    function getRandomPositionAlongPerimeter (): Feature<Point> {
+        return along(areaPerimeter as Feature<LineString>, randomGenerator.floatBetween(0, perimeterLength));
+    }
+
     // TODO fix number | undefined
     for (let i=0; i<allOptions.linesCount!; i++) {
+        const firstPosition = getRandomPositionAlongPerimeter();
+        let secondPosition = getRandomPositionAlongPerimeter();
+
+        // not generating lines which are perimeter segments
+        while (arePointsOnSameSegment(firstPosition, secondPosition, areaPerimeter as Feature<LineString>))
+            secondPosition = getRandomPositionAlongPerimeter();
+
         const line = lineString([
-            along(areaPerimeter as Feature<LineString>, randomGenerator.floatBetween(0, perimeterLength)).geometry.coordinates,
-            along(areaPerimeter as Feature<LineString>, randomGenerator.floatBetween(0, perimeterLength)).geometry.coordinates
+            firstPosition.geometry.coordinates,
+            secondPosition.geometry.coordinates
         ]);
 
         lines.push( line );
@@ -32,4 +44,13 @@ export function computeRandomLines (
     }
 
     return lines;
+}
+
+function arePointsOnSameSegment(
+    point1: Feature<Point>,
+    point2: Feature<Point>,
+    areaPerimeter: Feature<LineString>
+): boolean {
+    const perimeterSegment = lineSlice(point1.geometry.coordinates, point2.geometry.coordinates, areaPerimeter);
+    return perimeterSegment.geometry.coordinates.length === 2;
 }
