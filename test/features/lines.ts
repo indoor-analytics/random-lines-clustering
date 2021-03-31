35e090e84f9df1a50f643aafe6394475444fbc32
Alystrasz <raes.remy@gmail.com>
2021-03-31 18:22:49 +0200

    [test] adding test lines and runs

---
+++ test/features/lines.ts
@@ -0,0 +1,12 @@
+import {lineString} from "@turf/helpers";
+
+const randomLine1 = lineString([
+    [
+        3.069930885790112,
+        50.63755384359476
+    ],
+    [
+        3.069530411938615,
+        50.636565396795476
+    ]
+]);
