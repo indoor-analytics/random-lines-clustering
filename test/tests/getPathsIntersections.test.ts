5637e7e15563bfbbf0c02b298ec33de7a65a6545
Alystrasz <raes.remy@gmail.com>
2021-03-31 19:00:50 +0200

    [test] getPathsIntersections should build line with correct intersections

--- test/tests/getPathsIntersections.test.ts
+++ test/tests/getPathsIntersections.test.ts
@@ -3,10 +3,22 @@ import {actualFlandersRailway} from "../features/zones";
 import {randomLine1} from "../features/lines";
 import { expect } from "chai";
 import {mouais2run, mouaisRun} from "../features/runs";
+import booleanPointOnLine from "@turf/boolean-point-on-line";
 
 describe ('getPathsIntersections', () => {
    it ('should build intersection line', () => {
       const intersections = getPathsIntersections(actualFlandersRailway, [mouaisRun, mouais2run], [randomLine1]);
       expect(intersections.length).to.equal(1);
+      expect(intersections[0].line).to.deep.equal(randomLine1);
    });
+
+   it ('should build line with correct intersections', () => {
+      const intersections = getPathsIntersections(actualFlandersRailway, [mouaisRun, mouais2run], [randomLine1]);
+      const intersectLine = intersections[0];
+
+      expect(intersectLine.intersections.length).to.equal(6);
+
+      for (const intersection of intersectLine.intersections)
+         expect(booleanPointOnLine(intersection, intersectLine.line)).to.equal(true);
+   })
 });
