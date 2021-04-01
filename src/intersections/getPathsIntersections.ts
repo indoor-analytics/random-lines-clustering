68263cdf30a0d931245a93f5285c65f0b701ec49
Alystrasz <raes.remy@gmail.com>
2021-04-01 17:39:53 +0200

    [feat] adding basic getPathsIntersections implementation

--- src/intersections/getPathsIntersections.ts
+++ src/intersections/getPathsIntersections.ts
@@ -1,10 +1,21 @@
 import {IntersectionsLine} from "./IntersectionsLine";
-import {Feature, LineString, Polygon} from "@turf/helpers";
+import {Feature, LineString, Point, Polygon} from "@turf/helpers";
+import lineIntersect from "@turf/line-intersect";
 
 export function getPathsIntersections (
     zoneOfInterest: Feature<Polygon>,
     inputPaths: Feature<LineString>[],
     randomLines: Feature<LineString>[]
 ): IntersectionsLine[] {
-    return [];
+    return randomLines.map((line) => {
+        const intersections: Feature<Point>[] = [];
+
+        for (const inputPath of inputPaths) {
+            intersections.push(
+                ...lineIntersect(line, inputPath).features
+            );
+        }
+
+        return new IntersectionsLine(line, intersections)
+    });
 }
