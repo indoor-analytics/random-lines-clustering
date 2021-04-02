7bc41d0c5424d7ae4d97a774caada623c8bfca5e
Alystrasz <raes.remy@gmail.com>
2021-04-02 16:52:21 +0200

    [docs] adding getPathsIntersections documentation

--- src/intersections/getPathsIntersections.d.ts
+++ src/intersections/getPathsIntersections.d.ts
@@ -1,3 +1,9 @@
 import { IntersectionsLine } from "./IntersectionsLine";
 import { Feature, LineString } from "@turf/helpers";
+/**
+ * Computes intersections between randomly-generated lines and input paths.
+ *
+ * @param inputPaths
+ * @param randomLines
+ */
 export declare function getPathsIntersections(inputPaths: Feature<LineString>[], randomLines: Feature<LineString>[]): IntersectionsLine[];
--- src/intersections/getPathsIntersections.ts
+++ src/intersections/getPathsIntersections.ts
@@ -2,10 +2,18 @@ import {IntersectionsLine} from "./IntersectionsLine";
 import {Feature, LineString, Point} from "@turf/helpers";
 import lineIntersect from "@turf/line-intersect";
 
+
+/**
+ * Computes intersections between randomly-generated lines and input paths.
+ *
+ * @param inputPaths
+ * @param randomLines
+ */
 export function getPathsIntersections (
     inputPaths: Feature<LineString>[],
     randomLines: Feature<LineString>[]
 ): IntersectionsLine[] {
+
     return randomLines.map((line) => {
         const intersections: Feature<Point>[] = [];
 
