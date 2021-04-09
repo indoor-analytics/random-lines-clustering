13d992345f7eade625a5ae62512365961a4b0d24
Alystrasz <raes.remy@gmail.com>
2021-04-09 14:17:59 +0200

    [test] InputPath should add one intersection

--- test/tests/InputPath.test.ts
+++ test/tests/InputPath.test.ts
@@ -1,12 +1,23 @@
 import {InputPath} from "../../src/inputPath/InputPath";
 import {mouaisRun} from "../features/runs";
 import { expect } from "chai";
+import {point} from "@turf/helpers";
 
 describe ('InputPath class', () => {
     describe ('intersections', () => {
         it ('should not have any intersection when created', () => {
             const inputPath = new InputPath(mouaisRun);
             expect(inputPath.getIntersections().length).to.equal(0);
+        });
+
+        it ('should add one intersection', () => {
+            const inputPath = new InputPath(mouaisRun);
+            const intersection = point([2.3785655200481415, 51.04015939117184]);
+            inputPath.addIntersection(intersection);
+
+            const intersections = inputPath.getIntersections();
+            expect(intersections.length).to.equal(1);
+            expect(intersections).to.include(intersection);
         });
     });
 });
