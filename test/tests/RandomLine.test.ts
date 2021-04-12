880f145c4075346200f582b9380317d5adb2c3f6
Alystrasz <raes.remy@gmail.com>
2021-04-12 16:07:20 +0200

    [test] RandomLine should throw error with point not on random line

--- test/tests/RandomLine.test.ts
+++ test/tests/RandomLine.test.ts
@@ -1,6 +1,7 @@
 import {RandomLine} from "../../src/randomLine/RandomLine";
 import {slalomingAroundLineRun} from "../features/runs";
 import { expect } from "chai";
+import {point} from "@turf/helpers";
 
 describe ('RandomLine class', () => {
     describe ('instantiation', () => {
@@ -12,6 +13,16 @@ describe ('RandomLine class', () => {
         it ('should have empty clustered intersections map when created', () => {
             const line = new RandomLine(slalomingAroundLineRun);
             expect(line.clusteredIntersections).to.deep.equal({});
+        });
+    });
+
+    describe('get clustered intersection', () => {
+        it ('should throw error with point not on random line', () => {
+            const line = new RandomLine(slalomingAroundLineRun);
+            const outerPoint = point([50.636012, 3.073612]);
+            expect(
+                () => line.getClusteredIntersection(outerPoint)
+            ).to.throw(RangeError, 'Input point must belong to random line.');
         });
     });
 });
