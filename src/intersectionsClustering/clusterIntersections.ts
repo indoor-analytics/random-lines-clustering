d5a7a0962323d92c7e1050cb4d56677b11ee645a
Alystrasz <raes.remy@gmail.com>
2021-04-12 18:54:49 +0200

    [docs] adding TODO note

--- src/intersectionsClustering/clusterIntersections.ts
+++ src/intersectionsClustering/clusterIntersections.ts
@@ -3,5 +3,10 @@ import {IntersectionsMap} from "../intersectionsMap/IntersectionsMap";
 export function clusterIntersections (
     map: IntersectionsMap
 ): IntersectionsMap {
+
+    for (const randomLine of map.getAllIntersectionLines()) {
+        // TODO cluster line
+    }
+
     return map;
 }
