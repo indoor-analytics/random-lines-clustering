421a838d461ec2b55bd09736e08e26c08e5441a4
Alystrasz <raes.remy@gmail.com>
2021-04-12 17:44:14 +0200

    [test] RandomLine should throw error with point on line but not in intersections list

--- test/tests/RandomLine.test.ts
+++ test/tests/RandomLine.test.ts
@@ -2,6 +2,7 @@ import {RandomLine} from "../../src/randomLine/RandomLine";
 import {slalomingAroundLineRun} from "../features/runs";
 import { expect } from "chai";
 import {point} from "@turf/helpers";
+import pointToLineDistance from "@turf/point-to-line-distance";
 
 describe ('RandomLine class', () => {
     describe ('instantiation', () => {
@@ -23,6 +24,17 @@ describe ('RandomLine class', () => {
             expect(
                 () => line.getClusteredIntersection(outerPoint)
             ).to.throw(RangeError, 'Input point must belong to random line.');
+        });
+
+        it ('should throw error with point on line but not in intersections list', () => {
+            const line = new RandomLine(slalomingAroundLineRun);
+            const linePoint = point([3.072277307510376, 50.635539973727376]);
+            expect(pointToLineDistance(linePoint, slalomingAroundLineRun)).to.equal(0); // is a line point
+
+            expect(line.intersections.length).to.equal(0);
+            expect(
+                () => line.getClusteredIntersection(linePoint)
+            ).to.throw(RangeError, 'Input point must be a random line intersection.');
         });
     });
 });
