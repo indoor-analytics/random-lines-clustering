7ddbceb0044d4dc6ea24fb3f5ba6168ac207ab5d
Alystrasz <raes.remy@gmail.com>
2021-04-12 15:41:10 +0200

    [test] RandomLine should have empty clustered intersections map when created

--- test/tests/RandomLine.test.ts
+++ test/tests/RandomLine.test.ts
@@ -3,8 +3,15 @@ import {slalomingAroundLineRun} from "../features/runs";
 import { expect } from "chai";
 
 describe ('RandomLine class', () => {
-    it ('should have empty intersections list when created', () => {
-        const line = new RandomLine(slalomingAroundLineRun);
-        expect(line.intersections.length).to.equal(0);
+    describe ('instantiation', () => {
+        it ('should have empty intersections list when created', () => {
+            const line = new RandomLine(slalomingAroundLineRun);
+            expect(line.intersections.length).to.equal(0);
+        });
+
+        it ('should have empty clustered intersections map when created', () => {
+            const line = new RandomLine(slalomingAroundLineRun);
+            expect(line.clusteredIntersections).to.deep.equal({});
+        });
     });
 });
