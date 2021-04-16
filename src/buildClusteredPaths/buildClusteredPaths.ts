5444d8e9102a7e7e5f74cbb48d3055e727095b7f
Remy Raes <raes.remy@gmail.com>
2021-04-16 18:14:19 +0200

    [feat] adding buildClusteredPaths implementation

--- src/buildClusteredPaths/buildClusteredPaths.ts
+++ src/buildClusteredPaths/buildClusteredPaths.ts
@@ -1,4 +1,4 @@
-import {Feature, LineString} from "@turf/helpers";
+import {Feature, lineString, LineString} from "@turf/helpers";
 import {InputPath} from "../inputPath/InputPath";
 import {IntersectionsMap} from "../intersectionsMap/IntersectionsMap";
 
@@ -6,5 +6,28 @@ export function buildClusteredPaths (
     inputPaths: InputPath[],
     intersectionsMap: IntersectionsMap
 ) : Feature<LineString>[] {
-    return [];
+    const pathSegments: Feature<LineString>[] = [];
+
+    for (const path of inputPaths) {
+        let pathIntersections = path.getIntersections();
+        let currentIntersection = pathIntersections[0];
+        pathIntersections.shift();  // not comparing first point with itself
+
+        for (const intersection of pathIntersections) {
+            // TODO not push if segment already exists
+            pathSegments.push(
+                lineString([
+                    intersectionsMap.getLine(currentIntersection)
+                        .getClusteredIntersection(currentIntersection).geometry.coordinates,
+                    intersectionsMap.getLine(intersection)
+                        .getClusteredIntersection(intersection).geometry.coordinates
+                ])
+            );
+            currentIntersection = intersection;
+        }
+    }
+
+    // TODO link segments
+
+    return pathSegments;
 }
