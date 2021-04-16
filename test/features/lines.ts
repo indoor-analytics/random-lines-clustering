cc930efcec44239bb13f3e8e5d317e28bb86baae
Alystrasz <raes.remy@gmail.com>
2021-04-16 15:56:09 +0200

    [test] adding test features

--- test/features/lines.ts
+++ test/features/lines.ts
@@ -21,3 +21,25 @@ export const randomLine2 = lineString([
         50.63506023458684
     ]
 ]);
+
+export const citadelRandomLine1 = lineString([
+    [
+        3.0382776260375977,
+        50.642902164469874
+    ],
+    [
+        3.0512380599975586,
+        50.641731911797095
+    ]
+]);
+
+export const citadelRandomLine2 = lineString([
+    [
+        3.0451011657714844,
+        50.64535143619798
+    ],
+    [
+        3.0403804779052734,
+        50.63713226428859
+    ]
+]);
