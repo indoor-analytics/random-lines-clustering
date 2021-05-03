2467747948a561cd7e211fcae5546501390d8d54
Rémy Raes <remy.raes@inria.fr>
2021-05-03 14:04:12 +0200

    [test] kde should compute two intersection clusters with k=2

--- test/tests/kdeClustering.test.ts
+++ test/tests/kdeClustering.test.ts
@@ -5,9 +5,10 @@ import {kdeLineClusteringCore} from "../../src/intersectionsClustering/methods/k
 import { expect } from "chai";
 import {citadelRandomLine1, randomLine1} from "../features/lines";
 import {RandomLine} from "../../src/randomLine/RandomLine";
+import {kdeLineClustering} from "../../src/intersectionsClustering/methods/kde/control";
 
 describe ('KDE clustering', () => {
-    describe ('Core testing', () => {
+    describe ('Core', () => {
         // https://gist.github.com/Alystrasz/6ea1c20b0605cc0b482903b76cd5d716
         it ('should populate random line with clustered intersections', () => {
             const inputPath = new InputPath(slalomingAroundLineRun);
@@ -37,6 +38,36 @@ describe ('KDE clustering', () => {
             const fourthIntersection = intersections[3];
 
             kdeLineClusteringCore(firstLine, 81);
+
+            // first and second paths should point to one cluster
+            // third and fourth paths should point to another
+            const clusteredIntersection1 = firstLine.getClusteredIntersection(firstIntersection);
+            const clusteredIntersection2 = firstLine.getClusteredIntersection(secondIntersection);
+            const clusteredIntersection3 = firstLine.getClusteredIntersection(thirdIntersection);
+            const clusteredIntersection4 = firstLine.getClusteredIntersection(fourthIntersection);
+
+            expect(clusteredIntersection1).to.deep.equal(clusteredIntersection2);
+            expect(clusteredIntersection3).to.deep.equal(clusteredIntersection4);
+        });
+    });
+
+
+    describe ('controls', () => {
+        // https://gist.github.com/Alystrasz/a6b3599d5a16dafb5cd9f5648f0a3fdf
+        it ('should compute two intersection clusters with k=2', () => {
+            const inputPaths = citadelTopToBottomPaths.map((path) => new InputPath(path));
+            const intersectionsMap = getPathsIntersections(inputPaths, [new RandomLine(citadelRandomLine1)]);
+
+            // using first line (same line will appear four times)
+            const firstLine = intersectionsMap.getAllIntersectionLines()[0];
+            const intersections = firstLine.getIntersectionsList();
+            // because lines are conveniently sorted from left to right, intersections match lines
+            const firstIntersection = intersections[0];
+            const secondIntersection = intersections[1];
+            const thirdIntersection = intersections[2];
+            const fourthIntersection = intersections[3];
+
+            kdeLineClustering(firstLine, 2);
 
             // first and second paths should point to one cluster
             // third and fourth paths should point to another
