3f9dff53bd42b810efc6b0f62705fb50402974aa
Rémy Raes <remy.raes@inria.fr>
2021-05-03 12:10:33 +0200

    [docs] adding kde algo documentation

--- src/intersectionsClustering/methods/kde/control.d.ts
+++ src/intersectionsClustering/methods/kde/control.d.ts
@@ -1,2 +1,9 @@
 import { RandomLine } from "../../../randomLine/RandomLine";
+/**
+ * Regroups intersections on a random line via kernel-density estimation.
+ * This tries applying KDE with different bandwidth values until k value is satisfied.
+ *
+ * @param line intersections container
+ * @param k minimum count of intersections per cluster
+ */
 export declare function kdeLineClustering(line: RandomLine, k: number): void;
--- src/intersectionsClustering/methods/kde/control.ts
+++ src/intersectionsClustering/methods/kde/control.ts
@@ -1,8 +1,20 @@
 import {RandomLine} from "../../../randomLine/RandomLine";
 
+
+/**
+ * Regroups intersections on a random line via kernel-density estimation.
+ * This tries applying KDE with different bandwidth values until k value is satisfied.
+ *
+ * @param line intersections container
+ * @param k minimum count of intersections per cluster
+ */
 export function kdeLineClustering (
     line: RandomLine,
     k: number
 ): void {
+    // begin with a small bandwidth
 
+    // while k is not satisfied, increase bandwidth
+
+    // assign intersections to original random line
 }
